c807944381a69f37f7ad0cef6723a2db4fc75ccc
Rexy998 <159923347+Rexy998@users.noreply.github.com>
2024-05-02 22:17:19 -0400

    Add files via upload

---
+++ App2.java
@@ -0,0 +1,35 @@
+package newthing;
+import javax.swing.JOptionPane;
+
+public class App2 { 
+    
+
+
+    public static void main(String[] args) {
+
+        String pswd = "";
+
+        while (!pswd.equals("password123")) {
+        pswd = JOptionPane.showInputDialog("Enter the Password");
+        System.out.println("aaa" + pswd + "aaa");
+
+        }
+
+        JOptionPane.showMessageDialog(null, "Correct password");
+
+        Object[] gameslist = {"Trivia",
+                 "Survey", "Calendar", "Option4"};
+
+JOptionPane.showOptionDialog(null, //figure out how to harvest this value
+                 "Please select the program you'd like to use",
+                 "ProgramSelector3000",
+                 JOptionPane.YES_NO_OPTION,
+                 JOptionPane.INFORMATION_MESSAGE,
+                 null,
+                 gameslist,
+                 null);
+
+        trueOrFalse tfObject = new trueOrFalse();
+        tfObject.trueFalseGame();
+    }
+}
